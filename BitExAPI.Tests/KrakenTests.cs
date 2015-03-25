1074c989e1f3c6b37f60384896770034eca9561e
Steven Horn <horn@horndev.com>
2015-03-25 12:56:26 -0300

    Added test project for API

---
+++ BitExAPI.Tests/KrakenTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitExAPI.Tests
+{
+    [TestClass]
+    public class KrakenTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
