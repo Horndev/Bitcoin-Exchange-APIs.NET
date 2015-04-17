39a8476f623fe409e232cf863cc0c2fd3ccf5023
Steven Horn <horn@horndev.com>
2015-04-17 20:02:31 -0300

    added Time API Response

---
+++ BitExAPI/Markets/Kraken/TimeResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitExAPI.Markets.Kraken
+{
+    /// <summary>
+    /// response from https://api.kraken.com/0/public/Time
+    /// </summary>
+    public class TimeResponse : Response
+    {
+        public Resp result { get; set; }
+
+        public class Resp
+        {
+            public long unixtime { get; set; }
+            public string rfc1123 { get; set; }
+        }
+    }
+}
+
+//{"error":[],"result":{"unixtime":1429311294,"rfc1123":"Fri, 17 Apr 15 22:54:54 +0000"}}
