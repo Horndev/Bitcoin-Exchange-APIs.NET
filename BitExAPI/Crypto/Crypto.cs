a42226755e92cb3d40544ab8b6da421e51fa1475
Steven Horn <horn@horndev.com>
2015-05-15 20:58:00 -0300

    started adding structure for key encryption

---
+++ BitExAPI/Crypto/Crypto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitExAPI.Crypto
+{
+    /// <summary>
+    /// Manage the encryption and decryption of keys
+    /// </summary>
+    public class Crypto
+    {
+        public string Encrypt(string text, string key)
+        {
+            return "";
+        }
+
+        public string Decrypt(string cyphertext, string key)
+        {
+            return "";
+        }
+    }
+}
